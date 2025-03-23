d00651ba16311c566654d6ee0a02da62caa9b9a5
SiddhiChavan-26 <siddhichavan073@gmail.com>
2025-03-23 23:45:52 +0530

    Add files via upload

---
+++ decrement_overloading.cpp
@@ -0,0 +1,32 @@
+//program for operator overloading for unary minus operator
+#include<iostream>
+using namespace std;
+class Number{
+	private:
+		int value;
+		
+	public:
+		//constructor
+		Number(int v =0):value(v){
+			
+		}
+		
+		//operator overloading
+		Number operator --(){
+			return Number(--value);
+		};
+
+		//display function
+		void show(){
+			cout << "Changed value is: " << value << endl;
+		};
+};
+
+int main(){
+	Number n1(7);
+	Number n2 = --n1;
+	n2.show();
+	
+	
+	return 0;
+}
---
+++ division_overloading.cpp
@@ -0,0 +1,32 @@
+//program for operator overloading for division
+#include<iostream>
+using namespace std;
+class Point{
+	private:
+		int x_co;
+		int y_co;
+		
+	public:
+		//constructor
+		Point(int x=0, int y=0): x_co(x) , y_co(y){
+			
+		}
+		 //operator overloading
+		 Point operator /(const Point& obj){
+		 	return Point(x_co/obj.x_co , y_co/obj.y_co);
+		 }
+		 
+		 //display function
+		 void show(){
+		 	cout << "New value of x: " << x_co <<endl;
+			cout << "New value of y: " << y_co << endl;
+		 }
+		 
+};
+int main(){
+	Point p1(25,20) , p2(5,4);
+	Point p3 = p1/p2;
+	p3.show();
+	
+	return 0;
+}

f199a10cf7b8a82d146f762fe971c665ef30221f
Siddhi Chavan <siddhichavan073@gmail.com>
2025-05-29 20:12:02 +0530

    Add files via upload

---
+++ firstandLastpositionOftarget.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+using namespace std;
+
+int findFirst(const vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1, first = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            first = mid;
+            right = mid - 1; // search left half
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return first;
+}
+
+int findLast(const vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1, last = -1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            last = mid;
+            left = mid + 1; // search right half
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return last;
+}
+
+vector<int> searchRange(const vector<int>& nums, int target) {
+    return {findFirst(nums, target), findLast(nums, target)};
+}
+#include <iostream>
+
+int main() {
+    vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
+    vector<int> result = searchRange(nums, target);
+    
+    cout << "First and last position: [" << result[0] << ", " << result[1] << "]\n";
+    return 0;
+}
+
